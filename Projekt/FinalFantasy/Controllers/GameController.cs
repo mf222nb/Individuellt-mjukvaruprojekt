9ccebbe562ee9b45424c43f78a57f5570ba071b8
mf222nb <mf222nb@student.lnu.se>
2014-04-17 14:01:20 +0200

    Börjat med att göra en sida för ett spel

---
+++ Projekt/FinalFantasy/Controllers/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FinalFantasy.Models;
+
+namespace FinalFantasy.Controllers
+{
+    public class GameController : Controller
+    {
+        //
+        // GET: /Game/
+
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public ActionResult Details(int id) 
+        {
+            var site = new Site { Sida = "Sida " + id };
+
+            return View(site);
+        }
+    }
+}
