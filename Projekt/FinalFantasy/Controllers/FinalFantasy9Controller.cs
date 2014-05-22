1d418cecdcefd6afbfd1ea7a4cd40d730ef0ba6a
mf222nb <mf222nb@student.lnu.se>
2014-05-22 11:45:53 +0200

    gjort en till sida

---
+++ Projekt/FinalFantasy/Controllers/FinalFantasy9Controller.cs
@@ -0,0 +1,89 @@
+﻿using FinalFantasy.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinalFantasy.Controllers
+{
+    public class FinalFantasy9Controller : Controller
+    {
+        //
+        // GET: /FinalFantasy9/
+
+        FinalFantasyEntities db = new FinalFantasyEntities();
+
+        //Visar en sida med ett visst id
+        public ActionResult FinalFantasy9(int id)
+        {
+            var game = db.Sites.Find(id);
+            return View(game);
+        }
+
+        //Visar alla kommentarer som tillhör en viss sida
+        public ActionResult Details(int id)
+        {
+            var comment = db.Comments.Where(c => c.SiteID == id);
+            return View(comment);
+        }
+
+        //Sapa kommentarer
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //När man ska posta till databasen så tar denna hand om det
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Comments createComment)
+        {
+            if (ModelState.IsValid)
+            {
+                createComment.SiteID = 9;
+                db.Comments.Add(createComment);
+                db.SaveChanges();
+                return RedirectToAction("Details", new { id = "9" });
+            }
+            return View(createComment);
+        }
+
+        //Redigera kommentarer
+        public ActionResult Edit(int id)
+        {
+            var comment = db.Comments.Find(id);
+            return View(comment);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Comments editComment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(editComment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Details", new { id = "9" });
+            }
+            return View(editComment);
+        }
+
+        //Radera kommentarer
+        public ActionResult Delete(int id)
+        {
+            var comment = db.Comments.Find(id);
+            return View(comment);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var comment = db.Comments.Find(id);
+            db.Comments.Remove(comment);
+            db.SaveChanges();
+            return RedirectToAction("Details", new { id = "9" });
+        }
+    }
+}
